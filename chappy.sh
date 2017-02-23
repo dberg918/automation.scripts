0ff25e62a26b7ed33733c0cfc76af427c1b37075
David Berg <dberg918@gmail.com>
2017-02-23 15:36:40 +0900

    add support for point labels, add more test files
    chappy can now handle point labels, which are simply labels with no
    length (start and end times are equal). chappy subtracts 0.002s from
    the previous chapter's end time to prevent any overlap issues. this
    is only relevant for WebVTT files, as the chapters for M4As do not
    have end times.
    
    chappy can also detect overlapping chapters now and prints a warning
    for the offending label. again, only relevant for WebVTTs.

--- chappy.sh
+++ chappy.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
-
-if [ ! -f "$1" ]; then
-    echo
-    echo "Gimme a real file, kthnx."
-    exit
-fi
-
-CHAPTERS=${1:0:-4}.chapters
-VTT=${1:0:-4}.vtt
-
-echo "WEBVTT" > $VTT
-> $CHAPTERS
-i=1
+if [ ! -f "$1" ]; then echo -e "Gimme a real file, kthnx.\n"; exit; fi
+sh= ; sm= ; ss= ; eh= ; em= ; es= ;
+f=${1##*/}; CHAPTERS=${f:0:-4}.chapters; VTT=${f:0:-4}.vtt; VTTEMP=$VTT.tmp;
+echo "WEBVTT" > $VTT; > $CHAPTERS; > $VTTEMP; i=1;
 while IFS= read -r LINE; do
     start_seconds=`echo $LINE | cut -d ' ' -f 1 | rev | cut -c 4- | rev`
+    sh=`printf %02d $(bc <<< "$start_seconds / 3600")`
+    sm=`printf %02d $(bc <<< "$start_seconds / 60")`
+    ss=`printf %06.3f $(bc <<< "$start_seconds % 60")`
+    if [ "$i" -gt 1 ]; then
+        if (( `bc <<< "$start_seconds < $end_seconds"` )); then
+            echo -e "WARNING: chapter overlap at $chapter_name"
+        fi
+        ofs=`printf %06.3f $(bc <<< "$ss - 0.002")`
+        sed "s/$eh:$em:$es/$sh:$sm:$ofs/2" $VTTEMP >> $VTT
+    fi
     end_seconds=`echo $LINE | cut -d ' ' -f 2 | rev | cut -c 4- | rev`
+    eh=`printf %02d $(bc <<< "$end_seconds / 3600")`
+    em=`printf %02d $(bc <<< "$end_seconds / 60")`
+    es=`printf %06.3f $(bc <<< "$end_seconds % 60")`
     chapter_name=`echo $LINE | cut -d ' ' -f 3-`
-
-    sh=`printf %02d $(echo "$start_seconds / 3600" | bc)`
-    sm=`printf %02d $(echo "$start_seconds / 60" | bc)`
-    ss=`printf %06.3f $(echo "$start_seconds % 60" | bc)`
-    eh=`printf %02d $(echo "$end_seconds / 3600" | bc)`
-    em=`printf %02d $(echo "$end_seconds / 60" | bc)`
-    es=`printf %06.3f $(echo "$end_seconds % 60" | bc)`
-
-    echo -e "\n$i\n$sh:$sm:$ss --> $eh:$em:$es\n$chapter_name" >> $VTT
+    echo -e "\n$i\n$sh:$sm:$ss --> $eh:$em:$es\n$chapter_name" > $VTTEMP
     echo -e "CHAPTER$i=$sh:$sm:$ss\nCHAPTER"$i"NAME=$chapter_name" >> $CHAPTERS
     ((i=$i+1))
 done < $1
+cat $VTTEMP >> $VTT
+rm $VTTEMP
